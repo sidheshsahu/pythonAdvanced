0b90d9abc409889ce90d4c4ce036b50dd54576a1
sidheshsahu <sidhesh.sahu@somaiya.edu>
2026-06-16 21:22:05 +0530

    Added async

---
+++ ASYNC/async.py
@@ -0,0 +1,17 @@
+import asyncio
+import time
+
+# Coroutine function
+async def main():
+    print("Async Function") # This will print immediately
+    await asyncio.sleep(1)# The thread is idle here
+    print("Async Function after 1 second") # This will be executed right after the thread is idle
+
+
+asyncio.run(main())
+
+
+
+
+
+
